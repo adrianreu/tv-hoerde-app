015d8c1dc70f1bd793aec6ff460e34fba206e34a
adrianreutter <adrian.reutter@mail.de>
2023-06-04 12:56:26 +0200

    WIP: change request dialog

--- tvh-frontend/src/components/bookingPlan/InspectionDialog.vue
+++ tvh-frontend/src/components/bookingPlan/InspectionDialog.vue
@@ -46,21 +46,79 @@
               {{ booking?.bookedBy?.lastname }}
             </div>
           </div>
+          <div class="col-12" v-if="booking?.bookedBy?.id !== userId">
+            <q-expansion-item
+              v-model="expanded"
+              icon="ph-swap"
+              expand-icon="ph-caret-down"
+              expanded-icon="ph-caret-up"
+              label="Anfrage auf Übernahme"
+              class="overflow-hidden rounded-borders q-mt-md"
+              :class="{ 'shadow-1': expanded }"
+              header-class="bg-accent text-white"
+              expand-icon-class="text-white"
+            >
+              <q-card class="rounded-borders">
+                <q-card-section>
+                  <div class="text-weight-medium">
+                    Startzeit<span class="text-red">*</span>
+                  </div>
+                  <q-select
+                    :model-value="selectedStartTime"
+                    dense
+                    outlined
+                    :options="startTimeOptions"
+                    class="bg-white"
+                    clearable
+                    clear-icon="ph-x-circle"
+                    emit-value
+                    dropdown-icon="ph-caret-down"
+                    map-options
+                    @update:model-value="emits('update:selected-start-time', $event)"
+                  />
+                  <div class="text-weight-medium q-mt-md">
+                    Endzeit<span class="text-red">*</span>
+                  </div>
+                  <q-select
+                    :model-value="selectedEndTime"
+                    dense
+                    outlined
+                    :options="endTimeOptions"
+                    class="bg-white"
+                    clearable
+                    clear-icon="ph-x-circle"
+                    dropdown-icon="ph-caret-down"
+                    emit-value
+                    map-options
+                    :disable="selectedStartTime === null"
+                    @update:model-value="emits('update:selected-end-time', $event)"
+                  />
+                  <div class="text-weight-medium q-mt-md">
+                    Nachricht<span class="text-red">*</span>
+                  </div>
+                  <q-input
+                    dense
+                    type="textarea"
+                    outlined
+                    class="bg-white"
+                    clearable
+                    clear-icon="ph-x-circle"
+                  />
+                </q-card-section>
+                <q-btn class="full-width" unelevated color="primary" square>Anfrage absenden</q-btn>
+              </q-card>
+            </q-expansion-item>
+          </div>
         </div>
       </q-card-section>
       <q-card-actions class="row justify-end">
         <q-btn
-        v-if="booking?.bookedBy?.id === userId"
-        color="primary"
-        flat
-        @click="doDeleteBooking"
+          v-if="booking?.bookedBy?.id === userId"
+          color="primary"
+          flat
+          @click="doDeleteBooking"
         >Löschen</q-btn>
-        <q-btn
-        v-else
-        color="primary"
-        flat
-        @click="close"
-        >Anfrage</q-btn>
+
         <q-btn color="primary" flat @click="close">Schliessen</q-btn>
       </q-card-actions>
     </q-card>
@@ -93,6 +151,7 @@ const authStore = useAuthStore();
 
 const { userId } = storeToRefs(authStore);
 const loading = ref(false);
+const expanded = ref(false);
 
 function close() {
   emits('update:model-value', false);
